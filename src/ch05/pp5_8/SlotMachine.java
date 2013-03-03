c6a51162a109dea14185d928ca3a2df422a55ac0
Travis Mosley <tprowx@gmail.com>
2013-03-03 11:26:12 -0600

    beginning PP 5.8

---
+++ src/ch05/pp5_8/SlotMachine.java
@@ -0,0 +1,14 @@
+/* Copyright (C) 2013 Travis Mosley <tprowx@gmail.com>
+ *
+ * Design and implement an application that simulates a simple slot
+ * machine in which three numbers between 0 and 9 are randomly
+ * selected and printed side by side. Print an appropriate statement
+ * if all three of the numbers are the same, or if any two of the
+ * numbers are the same. Continue playing until the user chooses to
+ * stop.
+ */
+package ch05.pp5_8;
+
+public class SlotMachine
+{
+  
